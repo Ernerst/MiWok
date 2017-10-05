c4574abaa236010d927b0ec79dc48b6b6aaa3e7f
Ernerst <ernerstm@gmail.com>
2017-10-05 16:01:20 +0200

    Initial commit

---
+++ app/src/main/java/com/example/ekasilabalexcdtb/miwok/NumbersActivity.java
@@ -0,0 +1,22 @@
+package com.example.ekasilabalexcdtb.miwok;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class NumbersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_numbers);
+
+    }
+
+}
